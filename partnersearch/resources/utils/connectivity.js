ea9846da0d2dba3c0d4f3d03ff78156ba13ecb08
Nicolas <bussonnicolas@yahoo.fr>
2013-04-05 13:51:18 +0200

    ValidURL

--- partnersearch/resources/utils/connectivity.js
+++ partnersearch/resources/utils/connectivity.js
@@ -1,5 +1,5 @@
 
 //Root URL for the service
 
-var serviceUrl = "http://192.168.0.9:8000/sap/opu/odata/sap/Z_PARTNER_SRV_SRV/";
-//var serviceUrl = "proxy/http/192.168.0.9:8000/sap/opu/odata/sap/Z_PARTNER_SRV_SRV/";
+//var serviceUrl = "http://192.168.0.9:8000/sap/opu/odata/sap/Z_PARTNER_SRV_SRV/";
+var serviceUrl = "proxy/http/192.168.0.9:8000/sap/opu/odata/sap/Z_PARTNER_SRV_SRV/";
